f6f33e738b8ad717bc1e85e25c7c9a0dcb946c8e
slawek <slawek.grz@gmail.com>
2024-08-18 20:30:57 +0200

    move nodrigen requests logs to a volume and sync it to cloud watch

--- apps/sg-application/setup/setup.sh
+++ apps/sg-application/setup/setup.sh
@@ -4,5 +4,6 @@ SCRIPT_DIR=$(dirname -- $(realpath ${BASH_SOURCE}))
 source $SCRIPT_DIR/setup_directories.sh
 
 mkdir -p $SG_APPLICATION_BIBLE_FILES_DIR
+mkdir -p $SG_APPLICATION_NODRIGEN_REQUESTS_LOGS_DIR
 mkdir -p $SG_APPLICATION_LOGS_DIR
 mkdir -p $SG_APPLICATION_GATEWAY_DIR
