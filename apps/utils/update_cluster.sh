0aff50ce6a74c10f76bcf3dd83e2af90b46ff302
slawek <slawek.grz@gmail.com>
2024-09-14 15:18:46 +0200

    add sg-banks-application

--- apps/utils/update_cluster.sh
+++ apps/utils/update_cluster.sh
@@ -7,24 +7,24 @@ sleep 5
 core/management/stop_all.sh
 sleep 20
 
-rm -rf core
-rm -rf db
-rm -rf sg-application
+sudo rm -rf core
+sudo rm -rf db
+sudo rm -rf sg-application
 
-unzip -o docker.zip
-rm docker.zip
+sudo unzip -o docker.zip
+sudo rm docker.zip
 
-dos2unix core/**/* 2> /dev/null
-dos2unix db/**/* 2> /dev/null
-dos2unix secrets/**/* 2> /dev/null
-dos2unix sg-application/**/* 2> /dev/null
-dos2unix infrastructure/**/* 2> /dev/null
+sudo dos2unix core/**/* 2> /dev/null
+sudo dos2unix db/**/* 2> /dev/null
+sudo dos2unix secrets/**/* 2> /dev/null
+sudo dos2unix sg-application/**/* 2> /dev/null
+sudo dos2unix infrastructure/**/* 2> /dev/null
 
-chmod +x core/**/*
-chmod +x db/**/*
-chmod +x secrets/**/*
-chmod +x sg-application/**/*
-chmod +x infrastructure/**/*
+sudo chmod +x core/**/*
+sudo chmod +x db/**/*
+sudo chmod +x secrets/**/*
+sudo chmod +x sg-application/**/*
+sudo chmod +x infrastructure/**/*
 
 infrastructure/setup/setup.sh
 
