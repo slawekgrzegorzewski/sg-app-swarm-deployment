68d095fe92e63978d73d4b5c29fbdbe75d440b25
Sławek <slawek.grz@gmail.com>
2025-07-07 21:17:49 +0200

    worpress

--- apps/core/setup/setup.sh
+++ apps/core/setup/setup.sh
@@ -7,8 +7,6 @@ source $SECRETS_DIR/put_secrets_to_env.sh
 mkdir -p $CORE_GATEWAY_HTML_DIR
 mkdir -p $CORE_REGISTRY_DATA_DIR
 
-$SECRETS_DIR/setup_registry_auth.sh $REGISTRY_USER $REGISTRY_PASSWORD $CORE_CONFIG_DIR
-
 mkdir -p $REGISTRY_DATA_DIR
 mkdir -p $WORDPRESS_HTML_DIR
 
--- apps/utils/update_cluster.sh
+++ apps/utils/update_cluster.sh
@@ -9,6 +9,7 @@ sleep 20
 
 sudo rm -rf core
 sudo rm -rf db
+sudo rm -rf db_mysql
 sudo rm -rf sg-application
 
 sudo unzip -o docker.zip
@@ -16,12 +17,21 @@ sudo rm docker.zip
 
 sudo dos2unix core/**/* 2> /dev/null
 sudo dos2unix db/**/* 2> /dev/null
+sudo dos2unix db_mysql/**/* 2> /dev/null
 sudo dos2unix secrets/**/* 2> /dev/null
 sudo dos2unix sg-application/**/* 2> /dev/null
 sudo dos2unix infrastructure/**/* 2> /dev/null
 
+sudo chown slawek:slawek core
+sudo chown slawek:slawek db
+sudo chown slawek:slawek db_mysql
+sudo chown slawek:slawek secrets
+sudo chown slawek:slawek sg-application
+sudo chown slawek:slawek infrastructure
+
 sudo chmod +x core/**/*
 sudo chmod +x db/**/*
+sudo chmod +x db_mysql/**/*
 sudo chmod +x secrets/**/*
 sudo chmod +x sg-application/**/*
 sudo chmod +x infrastructure/**/*
