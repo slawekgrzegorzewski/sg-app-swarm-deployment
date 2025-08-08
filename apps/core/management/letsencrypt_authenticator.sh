a5110242c55a04b07242aec2161633243129fbc9
slawekgrzegorzewski <slawek.grzegorzewski@satago.com>
2025-08-08 12:30:56 +0200

    certbot auto verification

--- apps/core/management/letsencrypt_authenticator.sh
+++ apps/core/management/letsencrypt_authenticator.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(dirname -- $(realpath ${BASH_SOURCE}))
 
 source $SCRIPT_DIR/../setup/setup_directories.sh
 
-LETSENCRYPT_DIR=$CORE_GATEWAY_HTML_DIR/letsencrypt
-mkdir $LETSENCRYPT_DIR
+LETSENCRYPT_DIR=$CORE_GATEWAY_HTML_DIR/letsencrypt/.well-known/acme-challenge
+mkdir -p $LETSENCRYPT_DIR
 
 echo $CERTBOT_VALIDATION > $LETSENCRYPT_DIR/$CERTBOT_TOKEN
