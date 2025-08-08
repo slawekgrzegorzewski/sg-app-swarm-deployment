49e89e37ddaa750eee9cbee83199b15e09ae81db
slawekgrzegorzewski <slawek.grzegorzewski@satago.com>
2025-08-08 11:26:33 +0200

    certbot auto verification

---
+++ apps/sg-application/management/letsencrypt_authenticator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname -- $(realpath ${BASH_SOURCE}))
+
+source $SCRIPT_DIR/../setup/setup_directories.sh
+
+LETSENCRYPT_DIR=$($SG_APPLICATION_GATEWAY_DIR)/letsencrypt
+mkdir $LETSENCRYPT_DIR
+
+echo $CERTBOT_VALIDATION > $LETSENCRYPT_DIR/$CERTBOT_TOKEN
