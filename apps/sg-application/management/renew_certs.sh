ccfa6f28f91e76ca66ae57052a0fac45b1c260fb
Sławek <slawek.grz@gmail.com>
2025-10-24 19:25:51 +0200

    new domain in authenticator

--- apps/sg-application/management/renew_certs.sh
+++ apps/sg-application/management/renew_certs.sh
@@ -6,7 +6,7 @@ source $SCRIPT_DIR/../setup/setup_directories.sh
 
 LETSENCRYPT_CERTS_DIR=/etc/letsencrypt/live/grzegorzewski.pl
 
-sudo certbot certonly --manual --preferred-challenges http --manual-auth-hook $CORE_MANAGEMENT_DIR/letsencrypt_authenticator.sh --manual-cleanup-hook $CORE_MANAGEMENT_DIR/letsencrypt_cleanup.sh -d grzegorzewski.pl,be.grzegorzewski.pl,wordpress.grzegorzewski.pl
+sudo certbot certonly --manual --preferred-challenges http --manual-auth-hook $CORE_MANAGEMENT_DIR/letsencrypt_authenticator.sh --manual-cleanup-hook $CORE_MANAGEMENT_DIR/letsencrypt_cleanup.sh -d grzegorzewski.pl,be.grzegorzewski.pl,wordpress.grzegorzewski.pl,mail.grzegorzewski.pl
 sudo cp $LETSENCRYPT_CERTS_DIR/privkey.pem $SECRETS_DIR/certs/sgapplication2.key
 sudo cp $LETSENCRYPT_CERTS_DIR/fullchain.pem $SECRETS_DIR/certs/sgapplication2.crt
 sudo chown slawek:slawek $SECRETS_DIR/certs/sgapplication2.key
