fd560e5ac38d03c06c668a62b0eae42d65bf6543
Sławek <slawek.grz@gmail.com>
2026-06-04 16:39:36 +0200

    New postgres in 18.4 version

--- apps/db/management/backup_using_replica.sh
+++ apps/db/management/backup_using_replica.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+export PGPASSWORD=$(cat /run/secrets/postgres_replica_password)
+
 if [ ! -s /var/lib/postgresql/data/PG_VERSION ]; then
   echo "Initializing replica via basebackup..."
   pg_basebackup -h rpi5 -U replicator \
-    -D /backup/base_$(date date +"%Y-%m-%d-%H-%M-%S") \
+    -D /backup/base_$(date +"%Y-%m-%d-%H-%M-%S") \
     -Fp -Xs -P -R
 fi
 
