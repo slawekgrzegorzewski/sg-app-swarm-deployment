ef44ae89c0eb2b5f7910519655d89f9c64fa9e9b
Sławek <slawek.grz@gmail.com>
2026-06-04 16:58:12 +0200

    New postgres in 18.4 version

--- apps/db/management/backup_using_replica.sh
+++ apps/db/management/backup_using_replica.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -e
 
-export PGPASSWORD=$(cat /run/secrets/postgres_replica_password)
+export PGPASSWORD=$(cat /run/secrets/postgres_password)
+NOW=`date +"%Y-%m-%d_%H-%M-%S"`
+ACCOUNTANT_BACKUP_FILE_NAME=accountant_database_$NOW.sql
+BANKS_BACKUPS_FILE_NAME=banks_database_$NOW.sql
+SMART_HOME_BACKUPS_FILE_NAME=smart_home_database_$NOW.sql
 
 if [ ! -s /var/lib/postgresql/data/PG_VERSION ]; then
-  echo "Initializing replica via basebackup..."
-  pg_basebackup -h rpi5 -U replicator \
-    -D /backup/base_$(date +"%Y-%m-%d-%H-%M-%S") \
-    -Fp -Xs -P -R
-fi
+  pg_dump accountant -h rpi5 -U postgres -f /backup/$ACCOUNTANT_BACKUP_FILE_NAME
+  gzip -9 -c /backup/$ACCOUNTANT_BACKUP_FILE_NAME > /accountant_backups/$ACCOUNTANT_BACKUP_FILE_NAME.gz
+  rm -f /backup/$ACCOUNTANT_BACKUP_FILE_NAME
 
-exec docker-entrypoint.sh postgres
+  pg_dump banks -h rpi5 -U postgres -f /backup/$BANKS_BACKUPS_FILE_NAME
+  gzip -9 -c /backup/$BANKS_BACKUPS_FILE_NAME > /banks_backups/$BANKS_BACKUPS_FILE_NAME.gz
+  rm -f /backup/$BANKS_BACKUPS_FILE_NAME
+
+  pg_dump smart_home -h rpi5 -U postgres -f /backup/$SMART_HOME_BACKUPS_FILE_NAME
+  gzip -9 -c /backup/$SMART_HOME_BACKUPS_FILE_NAME > /smart_home_backups/$SMART_HOME_BACKUPS_FILE_NAME.gz
+  rm -f /backup/$SMART_HOME_BACKUPS_FILE_NAME
+fi
